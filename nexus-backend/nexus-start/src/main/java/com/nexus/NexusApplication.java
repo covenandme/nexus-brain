dc79af073c7afe14ff71368bb663c5d7de779f1e
Coven <covenandme0608@gmail.com>
2025-12-04 00:07:06 +0800

    new bran

--- nexus-backend/nexus-start/src/main/java/com/nexus/NexusApplication.java
+++ nexus-backend/nexus-start/src/main/java/com/nexus/NexusApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NexusApplication {
     public static void main(String[] args) {
         SpringApplication.run(NexusApplication.class, args);
-        short
     }
 }
