77d4a8953fa198dfdd9a085f447fc9289ddc5b2e
Coven <covenandme0608@gmail.com>
2025-12-04 21:41:45 +0800

    修改bug

--- nexus-backend/nexus-common/src/main/java/com/nexus/common/result/ResultCode.java
+++ nexus-backend/nexus-common/src/main/java/com/nexus/common/result/ResultCode.java
@@ -14,7 +14,7 @@ public enum ResultCode {
     VALIDATE_FAILED(400, "参数检验失败"),
     UNAUTHORIZED(401, "暂无登录或token已经过期"),
     FORBIDDEN(403, "没有相关权限"),
-    NOTFOUND(404,'请求的资源不存在'),
+    NOTFOUND(404,"请求的资源不存在"),
     
     /**服务端 */
     FAILED(500, "操作失败");
